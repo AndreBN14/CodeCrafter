749fb625a2a058016fa2f18fd4cfabe5f97edae3
KarloToro <135350154+KarloToro@users.noreply.github.com>
2024-10-14 08:43:07 -0500

    conexion a base de datos e implementacion de modelos de ER

--- Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/models.py
+++ Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import date, time
 # Create your models here.
+class Jugador(models.Model):
+    jugador_id = models.AutoField(primary_key=True)
+    usuario = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    pais = models.CharField(max_length=30)
+class Puntuacion(models.Model):
+    puntuaciones = models.PositiveBigIntegerField()
+    fecha = models.DateField(default=date.today)
+    dias = models.PositiveSmallIntegerField()
+    isTheBest = models.BooleanField()
+    recursos_criticos = models.JSONField(default=dict, blank=True)
+    jugador_id = models.ForeignKey(Jugador, on_delete=models.CASCADE,db_column="jugador_ID",related_name="puntajes")
+class Eventos_Base(models.Model):
+    evento_id=models.AutoField(primary_key=True)
+    descripcion = models.TextField() #aqui va todo el textazo que necesita gpt para tener contexto
+    elementos_afectados = models.JSONField(default=dict, blank=True)
+    #chin chen guan chin chin - _ - 
