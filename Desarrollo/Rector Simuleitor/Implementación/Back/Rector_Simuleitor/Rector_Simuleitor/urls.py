a8e642cb0383e3369933219aa68a8fd24ae97c38
S3RN4K <andrew.sernaq@gmail.com>
2024-10-22 19:56:06 -0500

    Fusionar cambios de la carpeta Back desde la rama Serna

--- Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Rector_Simuleitor/urls.py
+++ Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Rector_Simuleitor/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Apps.Loggin.views import register, login_view
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('loggin',login_view),
     path('registrarse',register),
+    path('', RedirectView.as_view(url='/loggin')), # Redirigir a loggin
 ]
 
 """
