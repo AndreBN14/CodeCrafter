24e5ccad8b473bded65690c2096ed84d4d751fe5
SernaAndrew <149739380+S3RN4K@users.noreply.github.com>
2024-11-09 16:28:29 -0500

    Add files via upload

---
+++ Desarrollo/Rector Simuleitor/Implementación/Front/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import { InitialLayout } from "../layouts/InitialLayout";
+import { Button } from "../components/ui/Button";
+import { FormField } from "../components/ui/FormField";
+
+export const Login = () => {
+  return (
+    <InitialLayout tittle="Iniciar Sesión" mt="130px">
+      <FormField label="Usuario" type="text" id="username" />
+      <FormField label="Contraseña" type="password" id="password" />
+      <div className="mt-24 flex w-full gap-2">
+        <Button width="half" to="/register">
+          Registrarse
+        </Button>
+        <Button width="half">Iniciar</Button>
+      </div>
+    </InitialLayout>
+  );
+};
