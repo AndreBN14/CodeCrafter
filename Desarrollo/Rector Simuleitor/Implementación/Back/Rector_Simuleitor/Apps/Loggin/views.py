d3ee4cc39f59ad0c6882e6999d6143ff4db8c21c
SernaAndrew <andrew.sernaq@gmail.com>
2024-11-12 13:00:32 -0500

    Implementación del register en backend
    Implementando los registros de usuarios, cambios en el usuario Jugador, especificando valores predefinidos y aumentando el tamaño de las contraseñas.

--- Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/views.py
+++ Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/views.py
@@ -1,17 +1,39 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.utils import timezone
 from Apps.Loggin.forms import CustomUserCreationForm
+from .models import Jugador
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = Jugador
+        fields = ['usuario', 'password1', 'password2', 'pais', 'email', 'first_name', 'last_name']
 
 def register(request):
     if request.method == 'POST':
+        print("Formulario enviado")
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            print("Formulario válido")
+            user = form.save(commit=False)            
+            user.is_active = True 
+            user.is_staff = False
+            user.is_superuser = False 
+            user.date_joined = timezone.now()
+            user.username = user.usuario
+            
+            user.save()
+            
             login(request, user)
-            return redirect('home')  # Cambia a la URL a la que quieres redirigir
+            return redirect('/')
+        else:
+            print("Formulario no válido")
+            print(form.errors)
     else:
         form = CustomUserCreationForm()
+    
     return render(request, 'register.html', {'form': form})
 
 def login_view(request):
