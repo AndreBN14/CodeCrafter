f27736129c8e60aa0b3a97716b58b1a9d50bf877
SernaAndrew <149739380+S3RN4K@users.noreply.github.com>
2024-11-12 21:07:32 -0500

    Update serializer.py

--- Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/serializer.py
+++ Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
 from .models import Jugador,Puntuacion,Eventos_Base
+from django.contrib.auth.hashers import make_password
+from .models import Jugador, Puntuacion, Eventos_Base
 
 class PuntuacionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,9 +9,20 @@ class PuntuacionSerializer(serializers.ModelSerializer):
         fields = '__all__'  # O puedes especificar una lista de campos
 
 class JugadorSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Jugador
-        fields = '__all__'  # O puedes especificar una lista de campos
+        fields = ['usuario', 'password', 'pais', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        # Asignar el campo 'usuario' a 'username'
+        validated_data['username'] = validated_data['usuario']
+        
+        # Hash de la contraseña antes de guardarla en la base de datos
+        validated_data['password'] = make_password(validated_data['password'])
+        user = Jugador.objects.create(**validated_data)
+        return user
 
 class EventosBaseSerializer(serializers.ModelSerializer):
     class Meta:
