6ae7f30a03c763fa129c8872c97c8787db320ab1
SernaAndrew <149739380+S3RN4K@users.noreply.github.com>
2024-11-12 21:08:23 -0500

    Update views.py

--- Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/views.py
+++ Desarrollo/Rector Simuleitor/Implementación/Back/Rector_Simuleitor/Apps/Loggin/views.py
@@ -1,9 +1,12 @@
 
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
-from Apps.Loggin.forms import CustomUserCreationForm
+from .serializer import JugadorSerializer
 from .models import Jugador
 
 class CustomUserCreationForm(UserCreationForm):
@@ -11,31 +14,22 @@ class CustomUserCreationForm(UserCreationForm):
         model = Jugador
         fields = ['usuario', 'password1', 'password2', 'pais', 'email', 'first_name', 'last_name']
 
+@api_view(['POST'])
 def register(request):
-    if request.method == 'POST':
-        print("Formulario enviado")
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            print("Formulario válido")
-            user = form.save(commit=False)            
-            user.is_active = True 
-            user.is_staff = False
-            user.is_superuser = False 
-            user.date_joined = timezone.now()
-            user.username = user.usuario
-            
-            user.save()
-            
-            login(request, user)
-            return redirect('/')
-        else:
-            print("Formulario no válido")
-            print(form.errors)
+    serializer = JugadorSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        user.is_active = True
+        user.is_staff = False
+        user.is_superuser = False
+        user.date_joined = timezone.now()
+        user.save()
+        
+        login(request, user)  # Autentica y crea la sesión
+        return Response({'message': 'Registro exitoso'}, status=status.HTTP_201_CREATED)
     else:
-        form = CustomUserCreationForm()
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    return render(request, 'register.html', {'form': form})
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
