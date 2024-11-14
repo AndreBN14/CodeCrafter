db5fbbfe1d99f876ed15c02daeffcbb2ccc3ed21
Owochan2004 <jhonridoutt@gmail.com>
2024-11-14 17:05:49 -0500

    Menu con efectos
    sonido

--- Desarrollo/Rector Simuleitor/Implementación/Front/src/pages/Menu.jsx
+++ Desarrollo/Rector Simuleitor/Implementación/Front/src/pages/Menu.jsx
@@ -1,17 +1,53 @@
 import { InitialLayout } from "../layouts/InitialLayout";
 import { Button } from "../components/ui/Button";
+import { useAuth } from "../store/useAuth";
+import { useNavigate } from "react-router-dom";
+import { useConfig } from "../store/useConfig";
 
 export const Menu = () => {
+  const { user, logout } = useAuth();
+  const { playSoundEffect } = useConfig();
+
+  const navigate = useNavigate();
+
   return (
-    <InitialLayout tittle="Rector Simulator" mt="80px" confb={true}>
-      <Button to="/game">Continuar</Button>
-      <Button to="/start">Nueva Partida</Button>
-      <Button to="/records">Mejores Records</Button>
-      <div className="flex w-full gap-2">
-        <Button width="half" to="/login">
-          Iniciar Sesión
+    <InitialLayout tittle="Rector Simulator" confb={true}>
+      <div className="mt-[100px] flex h-[60%] w-[80%] flex-col items-center md:mt-[120px] md:w-[50%] lg:w-[40%]">
+        <Button
+          onClick={() => {
+            playSoundEffect();
+            navigate("/game");
+          }}
+        >
+          Jugar
         </Button>
-        <Button width="half">Salir</Button>
+        <Button
+          onClick={() => {
+            playSoundEffect();
+            navigate("/records");
+          }}
+        >
+          Mejores Records
+        </Button>
+        {user ? (
+          <Button
+            onClick={() => {
+              playSoundEffect();
+              logout();
+            }}
+          >
+            Salir
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              playSoundEffect();
+              navigate("/login");
+            }}
+          >
+            Iniciar Sesión
+          </Button>
+        )}
       </div>
     </InitialLayout>
   );
