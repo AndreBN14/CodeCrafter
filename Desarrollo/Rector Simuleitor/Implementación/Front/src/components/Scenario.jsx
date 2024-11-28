c39526e13349f97ba98baba87aa31d45f3650bf4
KarloToro <135350154+KarloToro@users.noreply.github.com>
2024-11-28 10:24:27 -0500

    Mejora ahora los cuadros de decision y evento son adaptativos al contenido de la respueta de la API

--- Desarrollo/Rector Simuleitor/Implementación/Front/src/components/Scenario.jsx
+++ Desarrollo/Rector Simuleitor/Implementación/Front/src/components/Scenario.jsx
@@ -14,13 +14,68 @@ export const Scenario = () => {
     final = {
       name: "Sin dinero",
       img: om,
-      reason: "¡Se acabo el presupuesto!",
+      reason: "¡Se acabó el presupuesto!",
     };
   }
 
   if (people <= 0) {
     final = {
       name: "Sin personas",
+      img: str,
+      reason: "¡Se alza una huelga!",
+    };
+  }
+
+  return (
+    <section className="flex h-[55%] flex-col items-center justify-center bg-[#D9D9D9] p-8 font-s">
+      {/* Contenedor adaptable en ancho */}
+      <div className="mt-3 flex w-auto max-w-[90%] items-center justify-center rounded-[10px] bg-[#BA6060] p-4">
+        <div className="relative flex w-full">
+          <p className="w-full text-center text-white">
+            {final ? final.reason : event.evento}
+          </p>
+        </div>
+      </div>
+      <img
+        className="mt-4 h-4/5 w-[50%] object-contain"
+        src={final ? final.img : character.img}
+        alt={final ? final.name : character.name}
+      />
+      <h2 className="text-2xl text-[#BA6060]">
+        {final ? "Despedido" : character.name}
+      </h2>
+    </section>
+  );
+};
+
+
+
+
+/*
+Forma no adaptativa :(
+import { useResources } from "../store/useResources";
+import om from "../assets/finals/outOfMoney.svg";
+import str from "../assets/finals/strike.svg";
+import { useEvent } from "../store/useEvent";
+
+export const Scenario = () => {
+  const { money, people } = useResources();
+
+  const { event, character } = useEvent();
+
+  let final = null;
+
+  if (money <= 0) {
+    final = {
+      name: "Sin dinero",
+      img: om,
+      reason: "¡Se acabo el presupuesto!",
+    };
+  }
+
+  if (people <= 0) {
+    final = {
+      name: "Sin aprobación",
       img: str,
       reason: "¡Se alza una huelga!",
     };
@@ -44,3 +99,4 @@ export const Scenario = () => {
     </section>
   );
 };
+*/
